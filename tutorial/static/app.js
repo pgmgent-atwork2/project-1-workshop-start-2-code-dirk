035b77feec4d54b3709e1d84713717a25870fd4c
pgm-dirkbouckaert <dirk.bouckaert@student.arteveldehs.be>
2024-04-21 17:29:28 +0200

    Add tutorial

---
+++ tutorial/static/app.js
@@ -0,0 +1,28 @@
+(() => {
+  const app = {
+    init() {
+      this.numPages = 8;
+      this.cacheElements();
+      this.buildUI();
+    },
+    cacheElements() {
+      this.$slides = document.getElementById('slides');
+    },
+    async buildUI() {
+      for (let i = 1; i <= this.numPages; i++) {
+        const res = await fetch(`static/slides/${i}.html`);
+        const html = await res.text();
+        this.$slides.innerHTML += html;
+      }
+      Reveal.initialize({
+        controls: true,
+        progress: true,
+        center: true,
+        hash: true,
+        plugins: [RevealHighlight]
+      });
+      Reveal.slide(0);
+    },
+  };
+  app.init();
+})();
