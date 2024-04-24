9755a86f3c092e18c6cf0a74e30439c56cdff2d4
pgm-dirkbouckaert <dirk.bouckaert@student.arteveldehs.be>
2024-04-24 07:27:03 +0200

    Save theme in localStorage

--- tutorial/static/app.js
+++ tutorial/static/app.js
@@ -7,11 +7,15 @@
       this.listenForThemeToggle();
     },
     cacheElements() {
+      this.theme = localStorage.getItem('S2C-theme') || 'dark';
       this.$btnThemeToggle = document.getElementById('btn-theme-toggle');
       this.$placeholder = document.querySelector('.placeholder');
       this.$slides = document.getElementById('slides');
     },
     async buildUI() {
+      // Set theme
+      this.setTheme(this.theme);
+      // Add slides
       for (let i = 1; i <= this.numPages; i++) {
         const res = await fetch(`static/slides/${i}.html`);
         const html = await res.text();
@@ -29,16 +33,21 @@
     },
     listenForThemeToggle() {
       this.$btnThemeToggle.addEventListener('click', () => {
-        if (document.getElementById('theme-light')) {
-          document.getElementById('theme-light').remove();
-        } else {
-          const link = document.createElement('link');
-          link.rel = 'stylesheet';
-          link.href = 'static/revealjs/theme/beige.css';
-          link.id = 'theme-light';
-          document.head.appendChild(link);
-        }
+        if (this.theme === 'dark') this.setTheme('light')
+        else this.setTheme('dark')
       });
+    },
+    setTheme(theme) {
+      localStorage.setItem('S2C-theme', theme);
+      if (theme === 'dark') {
+        return document.getElementById('theme-light')?.remove();
+      } else {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = `static/revealjs/theme/beige.css`;
+        link.id = 'theme-light';
+        document.head.appendChild(link);
+      }
     }
   };
   app.init();
